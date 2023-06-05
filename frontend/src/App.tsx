5a4de37ac05b41af5a22aea5241482eac67adbf4
Ivan Hrytsenko <ivanhrytsenko23143@gmail.com>
2023-06-05 10:37:52 +0300

    done project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Controller } from "./components/controler";
+
+function App() {
+  return (
+    <>
+      <Controller />
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/recordMessage.tsx
@@ -0,0 +1,34 @@
+import React, { FC } from "react";
+import { ReactMediaRecorder } from "react-media-recorder";
+import { RecordIcon } from "./reactIcon";
+
+type RecordMessageProps = {
+  handleStop: any;
+};
+
+export const RecordMessage: FC<RecordMessageProps> = ({ handleStop }) => {
+  return (
+    <ReactMediaRecorder
+      audio
+      onStop={handleStop}
+      render={({ status, startRecording, stopRecording }) => (
+        <div className="mt-2">
+          <button
+            onMouseDown={startRecording}
+            onMouseUp={stopRecording}
+            className="bg-white p-4 rounded-full"
+          >
+            <RecordIcon
+              classText={
+                status === "recording"
+                  ? "animate-pulse text-red-500"
+                  : "text-sky-500"
+              }
+            />
+          </button>
+          <p className="mt-2 text-white font-light">{status}</p>
+        </div>
+      )}
+    />
+  );
+};
